b4d842526c37ec5bccfdf5f167bbfced839327e7
krish280706 <83763311+krish280706@users.noreply.github.com>
2021-05-16 16:49:25 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# c20-logic
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,33 @@
+var fixedRect,movingRect;
+
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(200, 200, 50, 80);
+  movingRect = createSprite(400, 200, 80, 30);
+  fixedRect.shapeColor = 'green'
+  movingRect.shapeColor = 'green'
+
+}
+
+function draw() {
+  background("black");  
+
+  movingRect.y = mouseY
+  movingRect.x = mouseX
+
+if(movingRect.x - fixedRect.x < fixedRect.width/2 + movingRect.width/2 && 
+    fixedRect.x - movingRect.x < fixedRect.width/2 + movingRect.width/2 &&
+     movingRect.y - fixedRect.y < fixedRect.height/2 + movingRect.height/2 &&
+     fixedRect.y - movingRect.y < fixedRect.height/2 + movingRect.height/2 ){
+
+  fixedRect.shapeColor = 'blue'
+  movingRect.shapeColor = 'blue'
+
+}
+else{
+  fixedRect.shapeColor = 'green'
+  movingRect.shapeColor = 'green'
+}
+
+  drawSprites();
+}
